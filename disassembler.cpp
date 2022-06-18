81f8a3df34a9abb550dc1f97e94884cd6039c90c
Toby M <n.tobias.melin@gmail.com>
2022-06-18 23:05:29 +0100

    Implement all 0x0 to 0x9 codes, plus a few bug fixes in output

--- disassembler.cpp
+++ disassembler.cpp
@@ -18,25 +18,50 @@ int main(int argc, char* argv[]) {
 		vector<unsigned char> fileData(fileSize);
 		chip8file.read((char*) &fileData[0], fileSize);
 
-		unsigned int addr1, addr2;
+		unsigned int addr1, addr2, addrCombined;
 		
 		for (int i = 0; i < fileSize; i += 2) {
-			printf("%.2X%.2X  -  ", fileData[i], fileData[i+1]);
+			addrCombined = (fileData[i] << 8) + fileData[i+1];
+			printf("%X   %X  -  ", i + 0x200, addrCombined);
 
 			switch (fileData[i] & 0xF0) {
+				case 0x00:
+					if (addrCombined == 0x00E0) {
+						printf("CLEAR SCREEN");
+					}
+					else if (addrCombined == 0x00EE) {
+						printf("return;");
+					}
+					else {
+						printf("CALL ROUTINE AT ADDRESS %.3X", addrCombined & 0xFFF);
+					}
+					break;
 				case 0x10:
-					printf("GOTO %d", (fileData[i] << 8) + fileData[i+1]);
+					printf("GOTO %X", ((fileData[i] & 0xF) << 8) + fileData[i+1]);
+					break;
+				case 0x20:
+					printf("*(0x%X)()", addrCombined & 0xFFF);
+					break;
+				case 0x30:
+					printf("Skip next if V%X == %X", (fileData[i] & 0xF), fileData[i+1]);
+					break;
+				case 0x40:
+					printf("Skip next if V%X != %X", (fileData[i] & 0xF), fileData[i+1]);
+					break;
+				case 0x50:
+					printf("Skip next if V%X = V%X", (fileData[i] & 0xF), (fileData[i+1] & 0xF0));
 					break;
 				case 0x60:
-					printf("V%.1X = %d", (fileData[i] & 0xF), fileData[i+1]);
+					printf("V%.1X = %X", (fileData[i] & 0xF), fileData[i+1]);
 					break;
 				case 0x70:
-					printf("V%.1X += %d", (fileData[i] & 0xF), fileData[i+1]);
+					printf("V%.1X += %X", (fileData[i] & 0xF), fileData[i+1]);
 					break;
 				case 0x80:
 					addr1 = fileData[i] & 0xF;
 					addr2 = (fileData[i+1] & 0xF0) >> 4;
 
+					// Last digit in 0x8 hex code determines operator
 					switch (fileData[i+1] & 0xF) {
 						case 0x0:
 							printf("V%.1X = V%.1X", addr1, addr2);
@@ -44,7 +69,34 @@ int main(int argc, char* argv[]) {
 						case 0x1:
 							printf("V%.1X |= V%.1X", addr1, addr2);
 							break;
+						case 0x2:
+							printf("V%.1X &= V%.1X", addr1, addr2);
+							break;
+						case 0x3:
+							printf("V%.1X ^= V%.1X", addr1, addr2);
+							break;
+						case 0x4:
+							printf("V%.1X += V%.1X", addr1, addr2);
+							// Set VF to 1 if there is a carry, else 0
+							break;
+						case 0x5:
+							printf("V%.1X -= V%.1X", addr1, addr2);
+							// Set VF to 0 if there is a borrow, else 1
+							break;
+						case 0x6:
+							printf("VF = V%.1X & 0xF, V%.1X >> 1", addr1, addr1);
+							break;
+						case 0x7:
+							printf("V%.1X = V%.1X - V%.1X", addr1, addr1, addr2);
+							// Set VF to 0 if there is a borrow, else 1
+							break;
+						case 0xE:
+							printf("VF = V%.1X & 0xF0, V%.1X << 1", addr1, addr1);
+							break;
 					}
+					break;
+				case 0x90:
+					printf("Skip next if V%X != V%X", addr1 & 0xF, (addr2 & 0xF0) >> 4)
 					break;
 			}
 
