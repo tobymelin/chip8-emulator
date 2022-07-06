1740749d3828721e343c12ca94b3737ee92c24d5
Toby M <n.tobias.melin@gmail.com>
2022-07-06 08:14:02 +0100

    Added skeleton code for CPU

---
+++ src/cpu.h
@@ -0,0 +1,16 @@
+#include "memory.h";
+
+class CPU {
+	private:
+		uint16_t* PC; // Program Counter (address register)
+		uint16_t* DT; // Timer
+		uint16_t* ST; // Sound timer
+		uint16_t* VX[16]; // 16-bit variable register V0 to VF
+		uint16_t* stack[16]; // Stack
+		uint16_t* SP; // Stack pointer
+
+		Memory* mem;
+ 	public:
+		CPU();
+};
+
