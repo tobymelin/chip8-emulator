85a65eddadfc543484ec0f4000a41596d3fb221d
Toby M <n.tobias.melin@gmail.com>
2022-07-10 11:38:32 +0100

    Add arg logic to main to load specific roms

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,14 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[]) {
 	CHIP8 *chip8 = new CHIP8();
 
-	chip8->load_rom("Pong [Paul Vervalin, 1990].ch8");
+ 	if (argc > 1)
+		chip8->load_rom(argv[1]);
+	else
+		chip8->load_rom("Pong [Paul Vervalin, 1990].ch8");
+
 	chip8->boot();
 }
 
